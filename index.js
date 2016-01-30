54484070e84a95ba9649a48b52c3bee34e048a20
HansHammel <oh.schitt@freenet.de>
2016-01-30 01:57:19 +0100

    updated bootstrap, jquery, etc.

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 /*jslint node:true,nomen:true*/
+
+/*
+D/ActivityManager
+D/CordovaActivity
+D/CordovaNetworkManager
+D/CordovaWebViewClient
+D/PanelView
+D/ResourcesManager
+D/XWalkActivity
+D/XWalkLib
+E/AndroidRuntime
+E/chromium
+E/Web Console
+I/ActivityManager
+I/App
+I/chromium
+I/InputReader
+I/Timeline
+I/WindowManager
+V/ActivityThread
+V/ApplicationPolicy
+V/KeyguardUpdateMonitor
+W/PackageManager
+W/PluginManager
+W/ResourcesManager
+W/System.err
+*/
+// filter only on CordovaLog debug messages skip all other
+// adb logcat CordovaLog:D *:S
+
 var util = require('util'),
 	colors = require('colors'),
+  os= require('os'),
+  express = require('express'),
 	app = require('express.io')(),
 	spawn = require('child_process').spawn,
     logcat = spawn('adb', ['logcat']),
 	state = {
-		'success': ['success', 'D\/DroidGap', 'D\/CordovaLog'],
-		'error': ['error', 'E\/'],
-		'warning': ['warning', 'W\/Web Console'],
-		'info': ['info']
+		'success': [/*'success',*/ 'D\/ActivityManager', 'D\/CordovaActivity', 'D\/CordovaNetworkManager', 'D\/CordovaWebViewClient', 'D\/PanelView', 'D\/ResourcesManager', 'D\/XWalkActivity', 'D\/XWalkLib', 'D\/CordovaLog'],
+		'danger': [/*'error',*/ /*'E\/',*/ 'E\/AndroidRuntime', 'E\/chromium', 'E\/Web Console'],
+		'warning': [/*'warning',*/ 'W\/Web Console', 'W\/System\.err', 'W\/ResourcesManager', 'W\/PackageManager'],
+		'info': [/*'info',*/ 'I\/ActivityManager', 'I\/App', 'I\/chromium', 'I\/InputReader', 'I\/Timeline', 'I\/WindowManager']
 	},
     parseStdout = function (data, _class) {
         'use strict';
-        data.toString().split('\n').forEach(function (line) {
+        data.toString().split(os.EOL).forEach(function (line) {
             if (line !== '') {
-                var type = ['info'];
+                var type = [''];
                 if (state.hasOwnProperty(_class)) {
                     type.push(_class);
                 } else {
@@ -29,35 +61,50 @@ var util = require('util'),
                         }
                     });
                 }
-                
-                if (type.indexOf('error') >= 0) {
+
+                if (type.indexOf('danger') >= 0) {
                     console.log(line.red.bold);
-                    app.io.broadcast('line', {'line': line, 'type': 'error'});
+                    app.io.broadcast('line', {'line': line, 'type': 'danger'});
                 } else if (type.indexOf('warning') >= 0) {
                     console.log(line.yellow.bold);
                     app.io.broadcast('line', {'line': line, 'type': 'warning'});
                 } else if (type.indexOf('success') >= 0) {
                     console.log(line.green.bold);
                     app.io.broadcast('line', {'line': line, 'type': 'success'});
-                } else {
+                } else if (type.indexOf('info') >= 0) {
                     console.log(line.blue.bold);
-                    app.io.broadcast('line', {'line': line, 'type': type[0]});
+                    app.io.broadcast('line', {'line': line, 'type': 'info'});
+                } else {
+                  //skip/filter leave the rest
+                    //console.log(line.white.bold);
+                    //app.io.broadcast('line', {'line': line, 'type': type[0]});
                 }
             }
         });
     };
 
 app.http().io();
+app.use('/vendor/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/vendor/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.get('/', function (req, res) {'use strict'; res.sendfile(__dirname + '/public/client.html'); });
 app.get('/js/jquery-1.9.1.min.js', function (req, res) {'use strict'; res.sendfile(__dirname + '/public/js/jquery-1.9.1.min.js'); });
 app.get('/js/bootstrap.min.js', function (req, res) {'use strict'; res.sendfile(__dirname + '/public/js/bootstrap.min.js'); });
 app.get('/css/bootstrap.min.css', function (req, res) {'use strict'; res.sendfile(__dirname + '/public/css/bootstrap.min.css'); });
 
-logcat.stdout.on('data', function (data) {'use strict'; parseStdout(data); });
-logcat.stderr.on('data', function (data) {'use strict'; parseStdout(data, 'error'); });
+var fs=require('fs');
+var serverAccessLogStream = fs.createWriteStream('log.log', {flags: 'w'}); //use a for append
+logcat.stdout.on('data', function (data) {'use strict';
+  //serverAccessLogStream.write(data);
+  parseStdout(data);
+});
+logcat.stderr.on('data', function (data) {'use strict';
+  //parseStdout(data, 'danger');
+});
 logcat.on('error', function (data) {
     'use strict';
-    parseStdout(data, 'error');
+    //parseStdout(data, 'danger');
 });
 
-module.exports = app;
+app.listen(3008);
+
+//module.exports = app;
