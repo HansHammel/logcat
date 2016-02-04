a12607dbcc8742fa2f1b956c4ece5afb6bdad515
HansHammel <oh.schitt@freenet.de>
2016-02-04 02:09:02 +0100

    fixed test

--- index.js
+++ index.js
@@ -37,103 +37,110 @@ which('adb', function (err /*,resolvedPath*/) {
         console.error('Install Android SDK or put adb on your path!');
         process.exit(1);
     }
-    var util = require('util'),
-        colors = require('colors'),
-        os = require('os'),
-        open = require('open'),
-        express = require('express'),
-        app = require('express.io')(),
-        spawn = require('child_process').spawn,
-        logcat = spawn('adb', ['logcat']),
-        state = {
-            'success': [/*'success',*/ 'D\/ActivityManager', 'D\/CordovaActivity', 'D\/CordovaNetworkManager', 'D\/CordovaWebViewClient', 'D\/PanelView', 'D\/ResourcesManager', 'D\/XWalkActivity', 'D\/XWalkLib', 'D\/CordovaLog'],
-            'danger': [/*'error',*/ /*'E\/',*/ 'E\/AndroidRuntime', 'E\/chromium', 'E\/Web Console'],
-            'warning': [/*'warning',*/ 'W\/Web Console', 'W\/System\.err', 'W\/ResourcesManager', 'W\/PackageManager'],
-            'info': [/*'info',*/ 'I\/ActivityManager', 'I\/App', 'I\/chromium', 'I\/InputReader', 'I\/Timeline', 'I\/WindowManager']
-        },
-        parseStdout = function (data, _class) {
-            'use strict';
-            data.toString().split(os.EOL).forEach(function (line) {
-                if (line !== '') {
-                    var type = [''];
-                    if (state.hasOwnProperty(_class)) {
-                        type.push(_class);
-                    } else {
-                        Object.keys(state).forEach(function (k) {
-                            if (util.isArray(state[k])) {
-                                state[k].forEach(function (rx) {
-                                    var r = new RegExp(rx);
-                                    if (r.test(line)) {
-                                        type.push(k);
-                                    }
-                                });
-                            }
-                        });
-                    }
-
-                    if (type.indexOf('danger') >= 0) {
-                        console.log(line.red.bold);
-                        app.io.broadcast('line', {'line': line, 'type': 'danger'});
-                    } else if (type.indexOf('warning') >= 0) {
-                        console.log(line.yellow.bold);
-                        app.io.broadcast('line', {'line': line, 'type': 'warning'});
-                    } else if (type.indexOf('success') >= 0) {
-                        console.log(line.green.bold);
-                        app.io.broadcast('line', {'line': line, 'type': 'success'});
-                    } else if (type.indexOf('info') >= 0) {
-                        console.log(line.blue.bold);
-                        app.io.broadcast('line', {'line': line, 'type': 'info'});
-                    } else {
-                        //skip/filter leave the rest
-                        //console.log(line.white.bold);
-                        //app.io.broadcast('line', {'line': line, 'type': type[0]});
-                    }
+});
+var util = require('util'),
+    colors = require('colors'),
+    os = require('os'),
+    open = require('open'),
+    express = require('express'),
+    app = require('express.io')(),
+    spawn = require('child_process').spawn,
+    logcat = spawn('adb', ['logcat']),
+    state = {
+        'success': [/*'success',*/ 'D\/ActivityManager', 'D\/CordovaActivity', 'D\/CordovaNetworkManager', 'D\/CordovaWebViewClient', 'D\/PanelView', 'D\/ResourcesManager', 'D\/XWalkActivity', 'D\/XWalkLib', 'D\/CordovaLog'],
+        'danger': [/*'error',*/ /*'E\/',*/ 'E\/AndroidRuntime', 'E\/chromium', 'E\/Web Console'],
+        'warning': [/*'warning',*/ 'W\/Web Console', 'W\/System\.err', 'W\/ResourcesManager', 'W\/PackageManager'],
+        'info': [/*'info',*/ 'I\/ActivityManager', 'I\/App', 'I\/chromium', 'I\/InputReader', 'I\/Timeline', 'I\/WindowManager']
+    },
+    parseStdout = function (data, _class) {
+        'use strict';
+        data.toString().split(os.EOL).forEach(function (line) {
+            if (line !== '') {
+                var type = [''];
+                if (state.hasOwnProperty(_class)) {
+                    type.push(_class);
+                } else {
+                    Object.keys(state).forEach(function (k) {
+                        if (util.isArray(state[k])) {
+                            state[k].forEach(function (rx) {
+                                var r = new RegExp(rx);
+                                if (r.test(line)) {
+                                    type.push(k);
+                                }
+                            });
+                        }
+                    });
                 }
-            });
-        };
 
-    app.http().io();
-    app.use('/vendor/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
-    app.use('/vendor/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-    app.get('/', function (req, res) {
-        'use strict';
-        res.sendfile(__dirname + '/public/client.html');
-    });
-    app.get('/js/jquery-1.9.1.min.js', function (req, res) {
-        'use strict';
-        res.sendfile(__dirname + '/public/js/jquery-1.9.1.min.js');
-    });
-    app.get('/js/bootstrap.min.js', function (req, res) {
-        'use strict';
-        res.sendfile(__dirname + '/public/js/bootstrap.min.js');
-    });
-    app.get('/css/bootstrap.min.css', function (req, res) {
-        'use strict';
-        res.sendfile(__dirname + '/public/css/bootstrap.min.css');
-    });
+                if (type.indexOf('danger') >= 0) {
+                    console.log(line.red.bold);
+                    app.io.broadcast('line', {'line': line, 'type': 'danger'});
+                } else if (type.indexOf('warning') >= 0) {
+                    console.log(line.yellow.bold);
+                    app.io.broadcast('line', {'line': line, 'type': 'warning'});
+                } else if (type.indexOf('success') >= 0) {
+                    console.log(line.green.bold);
+                    app.io.broadcast('line', {'line': line, 'type': 'success'});
+                } else if (type.indexOf('info') >= 0) {
+                    console.log(line.blue.bold);
+                    app.io.broadcast('line', {'line': line, 'type': 'info'});
+                } else {
+                    //skip/filter leave the rest
+                    //console.log(line.white.bold);
+                    //app.io.broadcast('line', {'line': line, 'type': type[0]});
+                }
+            }
+        });
+    };
 
-    var fs = require('fs');
-    //var serverAccessLogStream = fs.createWriteStream('log.log', {flags: 'w'}); //use a for append
-    logcat.stdout.on('data', function (data) {
-        'use strict';
-        //serverAccessLogStream.write(data);
-        parseStdout(data);
-    });
-    /*
-    logcat.stderr.on('data', function (data) {
-        'use strict';
-        //parseStdout(data, 'danger');
-    });
-    logcat.on('error', function (data) {
-        'use strict';
-        //parseStdout(data, 'danger');
-    });
-    */
+app.http().io();
+app.use('/vendor/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/vendor/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.get('/', function (req, res) {
+    'use strict';
+    res.sendfile(__dirname + '/public/client.html');
+});
+app.get('/js/jquery-1.9.1.min.js', function (req, res) {
+    'use strict';
+    res.sendfile(__dirname + '/public/js/jquery-1.9.1.min.js');
+});
+app.get('/js/bootstrap.min.js', function (req, res) {
+    'use strict';
+    res.sendfile(__dirname + '/public/js/bootstrap.min.js');
+});
+app.get('/css/bootstrap.min.css', function (req, res) {
+    'use strict';
+    res.sendfile(__dirname + '/public/css/bootstrap.min.css');
+});
 
-    app.listen(3000);
+var fs = require('fs');
+//var serverAccessLogStream = fs.createWriteStream('log.log', {flags: 'w'}); //use a for append
+logcat.stdout.on('data', function (data) {
+    'use strict';
+    //serverAccessLogStream.write(data);
+    parseStdout(data);
+});
+/*
+ logcat.stderr.on('data', function (data) {
+ 'use strict';
+ //parseStdout(data, 'danger');
+ });
+ logcat.on('error', function (data) {
+ 'use strict';
+ //parseStdout(data, 'danger');
+ });
+ */
+
+
+var server = app.listen(3000);
+if (!module.parent) {
+    console.log(server.address());
     console.log('Express listening on http://localhost:3000');
     console.log('visit http://localhost:3000 to see your filtred adb logs');
     open('http://localhost:3000');
+} else {
+    //for our tests
+    module.exports = server;
+}
 
-//module.exports = app;
-});
+
